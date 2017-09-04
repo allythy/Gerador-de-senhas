1f23eee513a5606a23a5d9652693a6951191a972
allythy <allythyrenan@gmail.com>
2017-09-03 22:55:53 -0300

    initial commit

---
+++ geradorSenha.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while [ "$menu" != "n" ]
+do
+  echo
+  read -p "Informe a quantidade de senhas: " qtdSenha
+  read -p "Informe a quantidade de caracteres da senha: " qtdCaracteres
+  echo
+  for i in $(seq $qtdSenha);
+    do
+      cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%&*+' | fold -w $qtdCaracteres | head -n1
+    done
+  echo
+  read -p "Deseja voltar ? [S/N]" menu
+  echo
+done
