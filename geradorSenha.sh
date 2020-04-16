c65bfa974476f688e17dfc6c3cf13910ee54c20f
MarlonWSantos <marlon.santos.santos@icen.ufpa.br>
2020-04-16 01:39:43 -0300

    Adicionado /usr/bin/env bash e indentação

--- geradorSenha.sh
+++ geradorSenha.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+##!/usr/bin/env bash
 while [ "$menu" != "n" -a "$menu" != "N" ]
 do
-  echo
-  read -p "Informe a quantidade de senhas: " qtdSenha
-  read -p "Informe a quantidade de caracteres da senha: " qtdCaracteres
-  echo
-  for i in $(seq $qtdSenha);
+	echo
+	read -p "Informe a quantidade de senhas: " qtdSenha
+	read -p "Informe a quantidade de caracteres da senha: " qtdCaracteres
+	echo
+	for i in $(seq $qtdSenha);
     do
-      cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%&*+' | fold -w $qtdCaracteres | head -n1
+		cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%&*+' | fold -w $qtdCaracteres | head -n1
     done
-  echo
-  read -p "Deseja voltar ? [S/N]" menu
-  echo
+	echo
+	read -p "Deseja voltar ? [S/n]" menu
+	echo
 done
